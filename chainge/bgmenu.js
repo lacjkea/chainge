17a676aed335250001cdcf836f1d9b2966cc0b40
Laura Vitali <lauravitali@MacBook-Pro-di-Laura.local>
2020-12-14 15:57:29 +0100

    open close bg menu without animations

--- chainge/bgmenu.js
+++ chainge/bgmenu.js
@@ -1,32 +1,46 @@
 const bgmenu = document.querySelector("#burger-menu");
 const nav = document.querySelector("#nav-mobile");
 const close = document.querySelector("#close-burger-menu");
+const header = document.querySelector("#mobile-menu-icons");
 
-bgmenu.addEventListener("click", openBgMenu);
+bgmenu.addEventListener("click", openNav);
 
-function openBgMenu() {
-    console.log("openBgMenu");
+function openNav() {
+    console.log("openNav");
 
     nav.classList.remove("hide");
+//    header.classList.add("white-background");
     bgmenu.classList.add("hide");
     close.classList.remove("hide");
-    close.addEventListener("click", closeBgMenu);
+    close.addEventListener("click", closeNav);
 
-//    bgmenu.classList.add("hide");
-//    close.classList.remove("hide");
-//    logo.classList.add("hide");
-//    login.classList.add("hide");
-//    close.addEventListener("click", closeBgMenu);
-//    pension.addEventListener("click", showSecNav);
+//    slideDown();
 }
 
-function closeBgMenu() {
-    console.log("closeBgMenu");
+//function slideDown() {
+//    console.log("slide down");
+//
+//    nav.classList.add("slide-down");
+//    close.addEventListener("click", slideUp);
+//}
 
-    nav.classList.add("hide");
-    bgmenu.addEventListener("click", openBgMenu);
+//function slideUp() {
+//    console.log("slide up");
+//
+//    nav.classList.add("slide-up");
+//    close.classList.add("hide");
+//    bgmenu.classList.remove("hide");
+//    header.classList.remove("white-background");
+//    bgmenu.addEventListener("click", openNav)
+//}
+
+function closeNav() {
+    console.log("close nav");
+
     close.classList.add("hide");
     bgmenu.classList.remove("hide");
+    nav.classList.add("hide");
+    bgmenu.addEventListener("click", openNav);
 }
 
 
